f6592a30a76091efb69ef7bf198d8f886a8248af
Gaurang Raval <129145863+GaurangRavalG@users.noreply.github.com>
2025-05-01 16:01:17 +0530

    Commit

---
+++ Module 2 - (30-4)/Area of Triangle.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("\n Area of Triangle");
+    
+    int side1 = 10, side2 = 25, side3 = 20;
+    
+    int ans = (side1 + side2 + side3) / 2;
+    printf("Area of Triangle is: %d", ans);
+    return 0;
+}
