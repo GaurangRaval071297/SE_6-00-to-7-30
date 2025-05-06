d5019a4fbbf85eac82b8f21107be267af37c0f5c
Gaurang Raval <129145863+GaurangRavalG@users.noreply.github.com>
2025-05-06 12:46:43 +0530

    Commit

---
+++ Module 2/6-5/Number_Pattern.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main() {
+    
+    int num, row, col;
+    
+    printf("Enter Number:- ");
+    scanf("%d", &num);
+    
+    for(row = 1; row <= num; row++) {
+        printf("\n");
+        for(col = 1; col <= row; col++) {
+            printf("%d", row);
+        }
+    }
+    
+    getch();
+}
